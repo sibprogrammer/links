44d161035fefb49da8f606b5c801ba204d12cff7
Alexei Yuzhakov <sibprogrammer@gmail.com>
2016-05-15 16:06:03 +0600

    Initial import of new version of the project.

---
+++ gulpfile.js
@@ -0,0 +1,104 @@
+/* jshint node: true */
+"use strict";
+
+let gulp = require('gulp');
+let concat = require('gulp-concat');
+let connect = require('gulp-connect');
+let open = require('gulp-open');
+let rsync = require('gulp-rsync');
+let browserify = require('browserify');
+let browserifyData = require('browserify-data');
+let source = require('vinyl-source-stream');
+let argv = require('yargs').argv;
+
+let config = {
+    devPort: 9000,
+    devBaseUrl: 'http://localhost',
+    paths: {
+        html: './src/*.html',
+        mainJs: './src/main.js',
+        js: [
+            './src/links.example.json'
+        ],
+        css: [
+            'node_modules/bootstrap/dist/css/bootstrap.min.css',
+            'node_modules/bootstrap/dist/css/bootstrap-theme.min.css',
+            './src/main.css'
+        ],
+        images: './src/images/**/*',
+        dist: './dist'
+    }
+};
+
+gulp.task('connect', () => {
+    connect.server({
+        root: ['dist'],
+        port: config.devPort,
+        base: config.devBaseUrl,
+        livereload: true
+    });
+});
+
+gulp.task('open', ['connect'], () => {
+    gulp.src('dist/index.html')
+        .pipe(open({ uri: config.devBaseUrl + ':' + config.devPort + '/'}));
+});
+
+gulp.task('html', () => {
+    gulp.src(config.paths.html)
+        .pipe(gulp.dest(config.paths.dist))
+        .pipe(connect.reload());
+});
+
+gulp.task('images', () => {
+    gulp.src(config.paths.images)
+        .pipe(gulp.dest(config.paths.dist + '/images'));
+});
+
+gulp.task('css', () => {
+    gulp.src(config.paths.css)
+        .pipe(concat('app.css'))
+        .pipe(gulp.dest(config.paths.dist + '/css'))
+        .pipe(connect.reload());
+});
+
+gulp.task('js', () => {
+    gulp.src(config.paths.js)
+        .pipe(gulp.dest(config.paths.dist + '/js'));
+
+    browserify(config.paths.mainJs)
+        .transform(browserifyData)
+        .bundle()
+        .on('error', console.error.bind(console))
+        .pipe(source('app.js'))
+        .pipe(gulp.dest(config.paths.dist + '/js'))
+        .pipe(connect.reload());
+});
+
+gulp.task('watch', () => {
+    gulp.watch(config.paths.html, ['html']);
+    gulp.watch(config.paths.css, ['css']);
+    gulp.watch(config.paths.mainJs, ['js']);
+});
+
+gulp.task('deploy', () => {
+    let settings = Object.assign({
+        host: undefined,
+        user: 'links',
+        dest: 'httpdocs/'
+    }, argv);
+
+    if ('undefined' === typeof argv.host) {
+        throw "Please specify host using --host parameter.";
+    }
+
+    return gulp.src('dist/**')
+        .pipe(rsync({
+            root: 'dist/',
+            hostname: settings.host,
+            username: settings.user,
+            destination: settings.dest
+        }));
+});
+
+gulp.task('default', ['html', 'css', 'js', 'images', 'open', 'watch']);
---
+++ src/main.js
@@ -0,0 +1,67 @@
+$ = jQuery = require('jquery');
+let bootstrap = require('bootstrap');
+
+$(function() {
+    let renderLinks = function(links) {
+        // last added, first shown
+        links = links.reverse();
+
+        let getLinkHtml = (link) => {
+            return `
+                <div class="col-xs-6 col-md-3">
+                    <a href="${link.url}" class="thumbnail" data-toggle="tooltip" data-placement="top" title="${link.description}">
+                        <img src="/images/shots/${link.shot}">
+                    </a>
+                    <div class="caption">
+                        <h5>${link.title}</h5>
+                    </div>
+                </div>
+            `;
+        };
+
+        for (let link of links) {
+            // use title in case of missed description
+            if ('undefined' === typeof link.description) {
+                link.description = link.title;
+            }
+
+            $('.container .row').append(getLinkHtml(link));
+        }
+
+        $('.search-query').on('keyup', (event) => {
+            let value = event.target.value.toLowerCase();
+
+            if (13 == event.keyCode) {
+                if ('' === value) {
+                    return;
+                }
+                let firstElement = $('.row > div:visible').get(0);
+                if (firstElement) {
+                    $(firstElement).children('a').get(0).click();
+                }
+                return;
+            }
+
+            $.each(links, (index, item) => {
+                let text = item.title + ' ' + item.url + ' ' + item.description;
+                let toggle = (text.toLowerCase().indexOf(value) < 0 ? 'hide' : 'show');
+                let currentElement = $($('.row > div').get(index));
+                currentElement[toggle]();
+            });
+        });
+
+        $('[data-toggle="tooltip"]').tooltip();
+        $('.search-query').focus();
+    };
+
+    $.getJSON('/js/links.json')
+        .done((data) => {
+            renderLinks(data.links);
+        })
+        .fail(() => {
+            $.getJSON('/js/links.example.json')
+                .done((data) => {
+                    renderLinks(data.links);
+                });
+        });
+}());
